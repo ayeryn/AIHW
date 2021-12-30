b1a777485ccf956be2394d9a27fc3f2b9c6733cc
ayeryn <erynli@utexas.edu>
2021-12-29 19:17:48 -0600

    3a completed

--- sentiment/submission.py
+++ sentiment/submission.py
@@ -17,12 +17,15 @@ def extractWordFeatures(x):
     """
     Extract word features for a string x. Words are delimited by
     whitespace characters only.
-    @param string x: 
+    @param string x:
     @return dict: feature vector representation of x.
     Example: "I am what I am" --> {'I': 2, 'am': 2, 'what': 1}
     """
     # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-    raise Exception("Not implemented yet")
+    d = collections.defaultdict(int)
+    for w in x.split():
+        d[w] += 1
+    return d
     # END_YOUR_CODE
 
 ############################################################
